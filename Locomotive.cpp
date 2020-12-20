2cfa5ffc355582a3f9f4bee0cb5259fc09862bf6
Alan Grier <griera@oregonstate.edu>
2020-12-20 14:20:02 -0800

    Format and add comments

--- Locomotive.cpp
+++ Locomotive.cpp
@@ -10,12 +10,14 @@ Locomotive::Locomotive(int address)
     ADDRESS = address;
 }
 
+// Set locomotive's direction to -1 (reverse)
 void Locomotive::reverse()
 {
-  direction = -1;
+    direction = -1;
 }
 
+// Set locomotive's direction to 1 (forward)
 void Locomotive::forward()
 {
-  direction = 1;
+    direction = 1;
 }
--- Locomotive.h
+++ Locomotive.h
@@ -11,11 +11,11 @@ class Locomotive
 private:
 public:
     Locomotive(int address);
-    void reverse();
-    void forward();
-    int ADDRESS;
-    int speed = 0;
-    int direction = 1;
+    void reverse();    // Change direction to -1
+    void forward();    // Change direction to 1
+    int ADDRESS;       // Positive number
+    int speed = 0;     // Positive number from 0 to 255
+    int direction = 1; // -1 for reverse, +1 for forward
 };
 
 #endif
