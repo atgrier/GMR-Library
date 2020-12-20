7f75d1a0a44052abd79a4c8ee3ec2c23679e9c84
Alan Grier <griera@oregonstate.edu>
2020-12-20 15:48:21 -0800

    Add a variety of methods to Locomotive class

--- Locomotive.cpp
+++ Locomotive.cpp
@@ -3,21 +3,48 @@
   Created by Alan T. Grier, 23 September 2019.
 */
 
+#include <arduino.h>
 #include "Locomotive.h"
 
-Locomotive::Locomotive(int address)
+Locomotive::Locomotive(int address, int ledPin, RHReliableDatagram *manager)
 {
-    ADDRESS = address;
+    _address = address;
+    _ledPin = ledPin;
+    _manager = manager;
 }
 
 // Set locomotive's direction to -1 (reverse)
 void Locomotive::reverse()
 {
-    direction = -1;
+    _direction = -1;
 }
 
 // Set locomotive's direction to 1 (forward)
 void Locomotive::forward()
 {
-    direction = 1;
+    _direction = 1;
+}
+
+// Set the speed of the locomotive
+void Locomotive::setSpeed(int speed)
+{
+    _speed = speed;
+}
+
+// Send throttle command with current speed and direction
+void Locomotive::sendThrottle()
+{
+    char pdata[3];
+    pdata[0] = 't';        // Throttle
+    pdata[1] = _speed;     // Speed
+    pdata[2] = _direction; // Direction
+    _manager->sendto((uint8_t *)pdata, strlen(pdata) + 1, _address);
+}
+
+// Send E-Stop command
+void Locomotive::sendEStop()
+{
+    char pdata[1];
+    pdata[0] = 'e'; // E-Stop
+    _manager->sendto((uint8_t *)pdata, strlen(pdata) + 1, _address);
 }
--- Locomotive.h
+++ Locomotive.h
@@ -6,16 +6,27 @@
 #ifndef Locomotive_h
 #define Locomotive_h
 
+#include <RHReliableDataGram.h>
+
 class Locomotive
 {
-private:
 public:
-    Locomotive(int address);
-    void reverse();    // Change direction to -1
-    void forward();    // Change direction to 1
-    int ADDRESS;       // Positive number
-    int speed = 0;     // Positive number from 0 to 255
-    int direction = 1; // -1 for reverse, +1 for forward
+    Locomotive(int address, int ledPin, RHReliableDatagram *manager);
+    void reverse();           // Change direction to -1
+    void forward();           // Change direction to 1
+    void setSpeed(int speed); // Set the speed of the locomotive
+    void sendThrottle();      // Send throttle command with current speed and direction
+    void sendEStop();         // Send E-Stop command
+    int speed() { return _speed; }
+    int direction() { return _direction; }
+    int ledPin() { return _ledPin; }
+
+private:
+    int _speed = 0;               // Positive number from 0 to 255
+    int _direction = 1;           // -1 for reverse, +1 for forward
+    int _address;                 // Positive number
+    int _ledPin;                  // Arudino PIN of status LED
+    RHReliableDatagram *_manager; // Radio manager
 };
 
 #endif
